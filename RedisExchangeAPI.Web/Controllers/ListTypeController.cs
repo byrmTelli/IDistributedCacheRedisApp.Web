adcb9ce08f05f1e67e017cb3cd2d443c93c5b8f6
Bayram <byrm.telli.contact@gmail.com>
2024-07-13 15:18:54 +0300

    redis listType methods usage

---
+++ RedisExchangeAPI.Web/Controllers/ListTypeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+
+        private string listKey = "usernames";
+        public ListTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(1);
+        }
+        public IActionResult Index()
+        {
+            List<string> usernames  = new List<string>();
+            if(_db.KeyExists(listKey))
+            {
+
+                _db.ListRange(listKey).ToList().ForEach(x =>
+                {
+                    usernames.Add(x.ToString());
+                });
+            }
+
+
+            return View(usernames);
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            //adds data to end, listLeftPush adds data to first index
+            _db.ListRightPush(listKey,name);
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(string name)
+        {
+            _db.ListRemove(listKey,name);
+            return RedirectToAction("Index");
+        }
+    }
+}
